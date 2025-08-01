a7e495c2fcb2d16ca49c289c3d2204b6705c8854
mohamed hamdi awad <mahmd.hamdii99@gmail.com>
2025-08-01 21:35:21 +0300

    Add project files.

---
+++ HotelApplication.Pl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelApplication.Pl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
