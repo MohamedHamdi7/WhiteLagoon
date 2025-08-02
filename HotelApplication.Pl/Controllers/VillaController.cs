0bb8607771eeb9f88f6f7b4ff5d44379dcc24e1e
mohamed hamdi awad <mahmd.hamdii99@gmail.com>
2025-08-02 20:31:51 +0300

    OnionArchiticature

---
+++ HotelApplication.Pl/Controllers/VillaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entites;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace HotelApplication.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public VillaController(ApplicationDbContext _applicationDbContext)
+        {
+            applicationDbContext = _applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var villa = applicationDbContext.Villas.ToList();
+            return View(villa);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa model)
+        {
+
+            return View();
+        }
+    }
+}
--- HotelApplication.Pl/Program.cs
+++ HotelApplication.Pl/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Infrastructure.Data;
+
 namespace HotelApplication.Pl
 {
     public class Program
@@ -8,6 +11,11 @@ namespace HotelApplication.Pl
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<ApplicationDbContext>(); 
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
